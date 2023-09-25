1cdb0a2bbd3a2047d27b4a82dda83dda28493225
flebu <spamacc733@gmail.com>
2023-09-25 22:50:17 +0300

    Introduced Dictionaries

--- phone.cpp
+++ phone.cpp
@@ -2,24 +2,49 @@
  
 #include <iostream>
 #include <fstream>
+#include <map>
 
-using std::cin;
-using std::string;
-using std::getline;
-using std::ofstream;
-using std::ifstream;
-using std::cout;
+using std::cin; 	using std::string; 
+using std::endl;  	using std::cout;
+using std::ofstream; 	using std::ifstream;
+using std::map; 	using std::pair;
+
+
 
 string contactName;
-string handleInput(){		//handle what action the user wants to take
-  auto input;
+  map<string,int> contacts;
+
+void Add() {
+     	int phoneNumber;
+	string name;
+	cout>>"enter Phone Name to Add: ";
+	cin<<name;
+	cout<<"enter Phone Number to Add: ";
+	cin>>phoneNumber;
+	//is the name not  in the dictionary?
+	if (contacts.find(name)!=contacts.end()){
+		contacts.insert(pair<string,int>(name,phoneNumber));
+	}else
+		cout<<"A contact with a similar name exists"<<endl;
+
+  
+}
+
+void handleInput(string c){//handles what action the user wants to take
+
+  int input;
   cin>>input;
+
+  if (input=='A' || input==5){//add items to the phoneBook 
+    Add();
+  }
+/*
   if (input==3 || input=="D" ){
 	  cout<<"\tselected Delete. Enter the Name to Delete: ";
 	  cin >>contactName;	//sets the global variable contactName
 	  Delete(contactName);
   }
- /* else if (input==1 || input=="V" )
+* else if (input==1 || input=="V" )
 	  View();
   else if (input==2 || input=="E" )
 	  Edit();
@@ -29,15 +54,18 @@ string handleInput(){		//handle what action the user wants to take
 	  Add();
 }*/
 
-void Delete(contactName){
+/*void Delete(contactName){
   //is the contactName in the phoneBook?
   //if not 
-}
+}*/
 
 int main(){
+	
   cout<<"\t\t1) View contact"<<endl;
   cout<<"\t\t2) Edit contact"<<endl;
   cout<<"\t\t3) Delete contact"<<endl;
   cout<<"\t\t4) Update contact"<<endl;
   cout<<"\t\t5) Add contact"<<endl;
+
+  handleInput(contactName);
 }
